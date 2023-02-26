74a48af3acaa93ebe8115e900b2cef235a874cd5
Justin Horner <justinh.horner@gmail.com>
2023-02-26 04:28:37 -0500

    Initial commit

---
+++ popos-workspace-config.sh
@@ -0,0 +1,6 @@
+gsettings set org.gnome.mutter dynamic-workspaces false
+gsettings set org.gnome.desktop.wm.preferences num-workspaces 4
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-1 "['<Super>1']"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-2 "['<Super>2']"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-3 "['<Super>3']"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-4 "['<Super>4']"
